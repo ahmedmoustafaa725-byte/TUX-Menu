f0921d49e18126a51416833f56d4310759b67e7a
ahmedmoustafaa725-byte <ahmedmoustafaa725@gmail.com>
2025-09-27 12:19:29 +0300

    Update firebase-init.js

--- firebase-init.js
+++ firebase-init.js
@@ -1,19 +1,38 @@
 import { initializeApp, getApp, getApps } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-app.js";
+
 import { getAuth } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-auth.js";
+
 import { getFirestore } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-firestore.js";
 
+
+
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
-  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
+
+  apiKey: "AIzaSyDqjdI3ZqFSVY_5Kmowak1DCOL5bZaCYoo",
+
+  authDomain: "tux-menu.firebaseapp.com",
+
+  projectId: "tux-menu",
+
+  storageBucket: "tux-menu.firebasestorage.app",
+
+  messagingSenderId: "326432857137",
+
+  appId: "1:326432857137:web:926cc2ffb364b2f5d44910",
+
+  measurementId: "G-1T1RRHCCDQ",
+
 };
 
+
+
 const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
+
 const auth = getAuth(app);
+
 const db = getFirestore(app);
 
+
+
 export { app, auth, db, firebaseConfig };
+
